b7bf95f813cbee15ec23a4889d00c3083619921e
Luis Vicente Ruiz Hernández <73003295+LuisVicente0901@users.noreply.github.com>
2023-09-06 16:26:42 -0600

    Add files via upload

--- Asegurado.java
+++ Asegurado.java
@@ -1,18 +1,11 @@
 
+import java.util.Scanner;
+
 public class Asegurado{
 // Atributos
 int edad;
 double salario, suma_asegurada, pago_seguro, probabilidad_siniestro;
 String nombre;    
-
-public Asegurado(){
-    edad=20;
-    salario=10000;
-    suma_asegurada=20000;
-    pago_seguro=500;
-    nombre="A";
-    probabilidad_siniestro = 0.7;
-    }
     
 public Asegurado(int edad,double salario, double suma_asegurada,
 double pago_seguro,String nombre, double probabilidad_siniestro){
@@ -24,6 +17,37 @@ double pago_seguro,String nombre, double probabilidad_siniestro){
     this.probabilidad_siniestro=probabilidad_siniestro;
 }
 
+ public Asegurado(){
+    
+    Scanner in = new Scanner(System.in);
+    System.out.println("Escribe el nombre del asegurado");
+    nombre=in.nextLine(); 
+        
+    System.out.println("Escribe la edad");
+    edad = in.nextInt();
+        
+    System.out.println("Escribe el salario");
+    salario=in.nextDouble();
+        
+    System.out.println("Escribe la suma asegurada");
+    suma_asegurada=in.nextDouble();
+        
+    System.out.println("Escribe el pago anual del seguro");
+    pago_seguro=in.nextDouble();
+            
+    System.out.println("Escribe la probabilidad de siniestración");
+    probabilidad_siniestro=in.nextDouble();
+    
+    this.nombre=nombre;
+    this.edad=edad;
+    this.salario=salario;
+    this.suma_asegurada=suma_asegurada;
+    this.pago_seguro=pago_seguro;
+    this.probabilidad_siniestro=probabilidad_siniestro;
+
+        }
+
+
 public boolean si_pagara(){
     if(salario>pago_seguro){
         return true;
@@ -31,7 +55,7 @@ public boolean si_pagara(){
     else{
         return false;
     }
-    
 }
+
     
 }
